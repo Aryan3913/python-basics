6ffa8d08bdfa9c0a47d4dcdeceb656bad8e4ad36
Aryan3913 <aryan.rangapur414@gmail.com>
2022-10-15 17:21:01 +0530

    first upload

---
+++ prjt.py
@@ -0,0 +1,14 @@
+def increment(num):
+    try:
+        a=int(input("Enter the number: "))
+        c=1/a
+        print(c)
+    except Exception as e:
+        print("lol, you have an error",e)
+    finally:
+        print("Thanks For Playing The Game!")
+
+increment('cool')
+
+
+
